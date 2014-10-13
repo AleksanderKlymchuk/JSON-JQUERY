74d8b84b4a199cf41bb72db6b3384e430a310fa1
OleksandrKL <olkly85@gmail.com>
2014-10-14 01:41:17 +0200

    CRUD

--- Movies/Models/Movie.cs
+++ Movies/Models/Movie.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieSPA.Models
 {
     public class Movie
     {
-        public int ID { get; set; }
+       
+        public int  ID { get; set; }
         public string Title { get; set; }
         public int Year { get; set; }
         public string Genre { get; set; }
--- Movies/Scripts/app.js
+++ Movies/Scripts/app.js
@@ -17,10 +17,11 @@
            }, 900)
     })
     var rating = new Array()
-    $.getJSON('api/movies', function(data)
+    var id = null;
+    $.getJSON('api/movies', function( data)
     {
         var genres = new Array();
-       
+        id=data[data.length -1].ID
         $
         $.each(data,function(key,value){
             if (genres.indexOf(value.Genre) === -1) {
@@ -32,7 +33,7 @@
                 rating.push(value.Rating)
             }
 
-
+  
         })
         $.each(genres, function (key, value) {
             var movies = $("<li><a class='thickbox' href='#ctable'></a></li>")
@@ -45,56 +46,52 @@
         $('#genres').after("<a class='thickbox' href='#addnew'  id='add'>Add&nbsp+<a>")
         $('#add').on('click', function (e) {
             $('#addnew').remove()
-            e.preventDefault()
+         
             var option
             $.each(rating, function (data, rat) {
-                option +=("<option>"+rat+"</option>")
+                option +=("<option >"+rat+"</option>")
 
             })
-            fr = ('<form id="addnew"><label for="gnr">Genre</label><input placeholder="Enter Genre" id="gnr" class="form-control"  /><label for="tl">Title</label><input placeholder="Enter Title" id="tl" class="form-control"  /><label for="yr">Year</label><input placeholder="Enter Year" id="yr" class="form-control"  /><br /><select>' + option + '</select><br /><br /><input id="sub" type="submit" value="Save" class="btn btn-success" class="form-control"  /></form>')
+            fr = ('<form id="addnew"><label for="gnr">Genre</label><input placeholder="Enter Genre" data="Genre" id="gnr" class="form-control"  /><label for="tl">Title</label><input data="Title" placeholder="Enter Title" id="tl" class="form-control"  /><label for="yr">Year</label><input placeholder="Enter Year" data="Year" id="yr" class="form-control"  /><br /><select data="Rating">' + option + '</select><br /><br /><input id="sub" type="submit" value="Save" class="btn btn-success" class="form-control"  /></form>')
             $('#tab').append(fr)
             $('#add').fancybox()
-           
-            $('#addnew').submit(function(e){
-                //e.preventDefault()
-                
-
+            $('#addnew').submit(function(){
                 $(':input').each(function () {
-                    
                     if ($(this).val() == "")
                     {
+                       
                         $(this).css("border-color", "red")
-                        check=false
-
                     }
                 })
                 if ($(':input').val()!="")
                 {
-
-                    alert('The data is saved')
+                  
+                    obj = new Object()
+                    obj.ID = id+1
+                    $.each($(':input').not('#sub'), function () {
+                    obj[$(this).attr('data')] = $(this).val()
+                    })
+                    $.ajax({
+                        type:"POST",
+                        url: "api/Movies",
+                        data: obj
+                    }, function (result) {
+                        alert(result)
+                    }
+                    )
+                    console.log(obj)
+                    //alert('The data is saved')
                 }
-               
             })
-           
             $(':input').each(function(){
-              
                 $(this).focus(function () {
-                    
                     $(this).css("border-color", "black")
-
                 })
-                
-
             })
-
-
         })
-
-    
-
         $("#genres li a").on("click", function (event) {
             $("li a").fancybox();
-            event.preventDefault();
+           
             $('table').remove()
             var tb = ("<table class='table table-bordered' id='ctable'><thead><tr><td>Title</td><td>Year</td><td>Rating</td><td>Edit  <input id='checkbox' type='checkbox' unchecked/></tr></thead><tbody><tr></tr></tbody></table>")
             $('#tab').append(tb)
@@ -108,12 +105,13 @@
             .promise()
             .done(function () {
                 $('#checkbox').change(function () {
+                   
                     tr = $('tbody tr')
                     td = tr.find('td').not('#last')
                     if (this.checked)
                     {
                        $('button').remove()
-                       tr.find('td:last').append('<button class="btn btn-success save ">Save</button>')
+                       tr.find('td:last').append('<button class="btn btn-success save ">Save</button><a class="glyphicon glyphicon-trash delete "></a>')
                        td.append('<input type="text" />')
                        td.each(function () {
                            $(this).find('input').attr({ value: $(this).text() })
@@ -128,15 +126,54 @@
                               obj.Genre = $(this).parent().parent().attr('data-genre')
                               obj.ID = $(this).parent().parent().attr('data')
                               obj[$(this).prev().attr('data-obj')] = ($(this).val())
+                             
                            })
                                $.ajax({
                                    type:"PUT",
                                    url: "api/movies/" + obj.ID,
                                    data:obj
                                }).done(function () {
-                                   alert("Data is saved")
+                                   cont=confirm("Data is saved!\n \n Do you want continue editing?")
+                                   if (cont==true)
+                                   {
+                                       
+
+                                   }
+                                   else
+                                   {
+
+                                       $.fancybox.close();
+                                   
+                                   }
+
+
                                })
-                           console.log(obj)
+                         
+                       })
+                       $('.delete').on('click', function () {
+
+                          
+
+                           var ers = confirm("Delete data?")
+                           if(ers==true){
+                               
+                               $.ajax({
+                                   type: "DELETE",
+                                   url: "api/movies/" + $(this).parent().parent().attr('data'),
+                                 
+                               }).done(function (msg) {
+
+
+                                   $.fancybox.close()
+                                   location.reload()
+                               })
+
+                              
+                           }
+                           else {
+
+                           }
+
                        })
                     }
                     else if (this.checked==false)
@@ -151,5 +188,7 @@
 
         })
     })
+
+
     /* This is basic - uses default settings */
 })(jQuery);
